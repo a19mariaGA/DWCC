e8fdb78e6aa17e467076eb0ba0d6d0baf3f34999
maria 1 <A19MariaGA@iessanclemente.net>
2023-11-03 17:27:07 +0100

    Ejercicio 2 en java completo

--- DWCC-03/Ejercicio2conJavascript/ejercicio2Java.js
+++ DWCC-03/Ejercicio2conJavascript/ejercicio2Java.js
@@ -2,10 +2,12 @@
 //Amosar o número de días que quedan dende hoxe (día actual) ata final de
 //curso (por exemplo, o 25 de Xuño). 
 
- // Obtener la fecha actual
+ // Obtener la fecha y hora actual
 let fechaActual = new Date();
 
+// a esta fecha le doy yo el valor 
 let finCurso = new Date(2024, 5, 25); // El mes sería 5 pq  serían indexados y empezamos en 0
+
 let diasqueFaltan = new Date(); 
 
 
@@ -31,12 +33,15 @@ document.write(" </br>");
 
 let diaCumpleaños = prompt("¿Qué día es tu cumpleaños?");
 let mesCumpleaños = prompt("¿Qué mes es tu cumpleaños?");
+
+// de la fecha actual me quedo solo con el año actual 
 let añoActual = new Date().getFullYear();
+
+// al mes se le resta 1 pq los meses en javaScrip empiezan en 0
 let cumpleaños = new Date(añoActual, mesCumpleaños - 1, diaCumpleaños);
 
-// Obtengo el día de la semana
 
-//let diaSemana = cumpleaños.getDay(); 
+ 
 
 function esFindeSemana() {
 
@@ -45,6 +50,7 @@ function esFindeSemana() {
         cumpleaños.setFullYear(añoActual);
   
       // Verificar si el día de cumpleaños es fin de semana (0 = domingo, 6 = sábado)
+      // con getDay me quedo solo con el dia 
       if (cumpleaños.getDay() === 0 || cumpleaños.getDay() === 6) {
 
         document.write("Tu cumpleaños coincide en fin de semana los años: " + añoActual + "</br>");
@@ -57,24 +63,24 @@ function esFindeSemana() {
   document.write(" </br>"); 
   document.write(" </br>"); 
 
-  let opcionFormato = parseInt(prompt("Selecciona un formato de fecha:\n 1. 17/02/2016\n 2. dia de la semana, dia mes año \n 3. Wednesday, February 17, 2016"));
-  var fechaHoy = new Date();
+  let opcionFormatoFecha = parseInt(prompt("Selecciona un formato de fecha:\n 1. 17/02/2016\n 2. Mércores, 17 de febreiro de 2016 \n 3. Wednesday, February 17, 2016"));
+  //var fechaHoy = new Date();
 
 
   function formatearFecha(opcion) {
 
     switch (opcion) {
         case 1:
-            // Formato: 17/02/2016
-            document.write(fechaHoy.toLocaleDateString('es-ES'));
+            //  17/02/2016
+            document.write(fechaActual.toLocaleDateString('es-ES'));
             break;
         case 2:
-            // Formato: Mércores, 17 de febreiro de 2016
-            document.write(fechaHoy.toLocaleDateString('gl-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }));
+            //  Mércores, 17 de febreiro de 2016
+            document.write(fechaActual.toLocaleDateString('gl-ES', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }));
             break;
         case 3:
             // Formato: Wednesday, February 17, 2016
-            document.write(fechaHoy.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }));
+            document.write(fechaActual.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }));
             break;
         default:
             document.write('No ha elegido la opción correcta');
@@ -82,8 +88,44 @@ function esFindeSemana() {
 }
 
 // Llamada a la función
-formatearFecha(opcionFormato);
+formatearFecha(opcionFormatoFecha);
 
 
 document.write(" </br>"); 
 document.write(" </br>"); 
+
+
+/*Amosa a hora actual en diferentes formatos segundo o valor que meta o
+usuario por parámetro (usa un
+switch):
+1.14:35:07 (hora detallada con minutos e segundos)
+2.02:35 PM ou 02:35 AM (hora con minutos e AM ou PM segundo sexa antes
+ou despois de mediodía)*/
+
+
+let horaActual = fechaActual.getHours();  // me quedo con la hora de la fecha actual 
+let minutoActual = fechaActual.getMinutes(); // me quedo con los minutos de la fecha actual 
+let segundoActual = fechaActual.getSeconds(); // me quedo con los segundos de la fecha actual 
+let ampm = horaActual >= 12 ? 'PM' : 'AM';
+
+let opcionFormatoHora = parseInt(prompt("Selecciona un formato de hora:\n 1. 14:35:07 \n 2. 02:35 PM ou 02:35 AM"));
+
+
+function obtenerHora(opcion2) {
+  switch (opcion2) {
+    case 1:
+      // 14:35:07
+      document.write(horaActual +":" + minutoActual+":" +segundoActual);
+      break;
+    case 2:
+      // 02:35 PM ou 02:35 AM
+       if (horaActual > 12) horaActual -= 12;
+       document.write(`${horaActual}:${minutoActual}:${segundoActual} ${ampm}`);
+  
+      break;
+    default:
+      document.write("No ha elegido la opción correcta");
+  }
+}
+
+obtenerHora(opcionFormatoHora);
