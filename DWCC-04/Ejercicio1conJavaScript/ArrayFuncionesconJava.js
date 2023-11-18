e99cec879773464ca916a5d464b4c599080b34d5
maria 1 <A19MariaGA@iessanclemente.net>
2023-11-18 20:15:51 +0100

    Unidad4

---
+++ DWCC-04/Ejercicio1conJavaScript/ArrayFuncionesconJava.js
@@ -0,0 +1,49 @@
+
+// Creamos el array 
+
+let paises = ["España","Italia","Francia","Marruecos","Turkia", "Grecia"]
+
+//Amosa o número de elementos do Array
+
+document.write("El número de elementos que hay en el array es de: " + paises.length + "<br>");
+
+//Amosa todos os países
+
+
+for (let i in paises){
+
+   
+    document.write (paises[i] + ", ");
+
+}
+
+document.write ( "</br>");
+//Amosa todos os países en sentido inverso. Ollo, que o Array
+//orixinal non quede modificado, é dicir, que non quede ordenado ao revés.
+
+
+let paisesInverso = paises.slice().reverse();
+document.write( paisesInverso + ", ");
+
+
+//Engade un país ao comezo do Array
+
+document.write ( "</br>");
+
+document.write ("Ahora el número de paises son: " + paises.unshift("Irlanda")+" = ");
+
+for (let i in paises){
+ document.write ( paises[i] + ", ");
+}
+
+
+//Engade un país ao final do Array
+
+document.write ( "</br>");
+
+document.write ("Ahora el número de paises son: " + paises.push("Alemania")+" = ");
+
+for (let i in paises){
+ document.write ( paises[i] + ", ");
+}
+
