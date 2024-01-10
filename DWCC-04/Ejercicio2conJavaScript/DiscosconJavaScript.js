cf59c809a64fdf528bc19f967426140988670aba
maria 1 <A19MariaGA@iessanclemente.net>
2024-01-10 21:15:00 +0100

    nuevo borrador para el ejercicio discos con java

--- DWCC-04/Ejercicio2conJavaScript/DiscosconJavaScript.js
+++ DWCC-04/Ejercicio2conJavaScript/DiscosconJavaScript.js
@@ -16,62 +16,103 @@ class Disco {
   }
 
 
+     // Getters
+     get Nome() { return this.nome; }
+     get Grupo() { return this.grupo; }
+     get Ano() { return this.ano; }
+     get Tipo() { return this.tipo; }
+     get Prestado() { return this.prestado; }
+   
+     // Setters
+     set Nome(n) { this.nome = n; }
+     set Grupo(g) { this.grupo = g; }
+     set Ano(a) { this.ano = a; }
+     set Tipo(t) { this.tipo = t; }
+     set Prestado(p) { this.prestado = p; }
+
+     
+
   mostrarInformacion() {
     document.write("Nome do disco:", this.nome, "<br>");
     document.write("Grupo de música ou cantante:", this.grupo,"<br>");
     document.write("Ano de publicación:", this.ano, "<br>");
-    document.write("Tipo de música:", this.tipoMusica, "<br>");
+    document.write("Tipo de música:", this.tipo, "<br>");
     document.write("Prestado:", this.prestado ? "Si" : "Non", "<br>");
-    document.write("------ --------------------------<br>");
+    document.write("------ --------------------------<br><br>");
 }
 
-  infoDisco(disco) {
-    return  document.write (`Nome do disco: ${this.nome}, Grupo: ${this.grupo}, Ano de publicación: ${this.ano}, Tipo de música: ${this.tipo}, Prestado: ${this.prestado ? 'Si' : 'No'}, <br><br><br>`);
+  infoDisco() {
+    return  document.write (`Nome do disco: ${this.nome}, Grupo: ${this.grupo}, Ano de publicación: ${this.ano}, Tipo de música: ${this.tipo}, Prestado: ${this.prestado ? 'Si' : 'No'}, <br>`);
   }
 
 }
 
 let disco1 = new Disco("Chenoa", "Chenoa", 2019, "pop", false);
 let disco2 = new Disco("Bisbal", "Bisbal", 2020, "latino", true);
-let disco3 = new Disco("Alejandro Sanz", "Corazon Partido", 2018, "romantico", true);
+let disco3 = new Disco("Corazon Partido", "Alejandro Sanz", 2018, "romantico", true);
 let disco4 = new Disco("Izal", "Que Bien", 2021, "indi", false);
 
 
-// Función para cargar datos de un disco
-function cargarDisco(disco, nome, grupo, ano, tipoMusica, prestado) {
-  disco.nome = nome;
-  disco.grupo = grupo;
-  disco.ano = ano;
-  disco.tipoMusica = tipoMusica;
-  disco.prestado = prestado;
+
+function cargarDisco() {
+  let agregarMas;
+  let discos = [];
+  discos = [disco1, disco2, disco3, disco4];
+
+
+  do {
+    let nome = prompt("Cal e o nome do disco que queres dar de alta?");
+    let grupo = prompt("Cal e o nome do grupo ou do cantante?");
+    let ano = parseInt(prompt("Ano de publicación:"));
+    let tipo = prompt("Tipo de música:");
+    let prestado = prompt("¿Está prestado? (Si/No)").toLowerCase() === 'si';
+
+    let nuevoDisco = new Disco(nome, grupo, ano, tipo, prestado);
+    discos.push(nuevoDisco);
+
+    agregarMas = prompt("¿Quieres añadir más discos? (Si/No)").toLowerCase();
+  } while (agregarMas === 'si');
+
+  return discos;
 }
 
-let disco5= new Disco ();
-let disco6= new Disco ();
-
-// Cargar datos de los discos utilizando la función
-cargarDisco(disco5, "Dani Martin", "Cero", 2015, "Pop", false);
-cargarDisco(disco6, "Vetusta Morla", "Valiente", 2018, "Pop", true);
+let discos = cargarDisco();
 
 
 document.write("<br> <br>");
-document.write ("lOS DATOS DEL DISCO1 SON: <br><br>");
-console.log(disco1.infoDisco());
+document.write("LOS DATOS DEL DISCO1 SON: <br><br>");
+console.log(disco1.mostrarInformacion());
 console.log("<br> <br> <br>");
 
-
-
-// Creamos un array
-let discos = [disco1, disco2, disco3, disco4, disco5, disco6];
-
 function mostrarTodosLosDiscos() {
-  document.write("LOS DATOS DE TODOS LOS DISCOS SON:"+ "<br><br>");
+  document.write("LOS DATOS DE TODOS LOS DISCOS SON:" + "<br><br>");
   discos.forEach(disco => {
-    
-    console.log(disco.mostrarInformacion()+ "<br>");
-    console.log('------------------------');
+    disco. infoDisco();
+  
   });
 }
 
-// Llama a la función para mostrar la información de todos los discos
 mostrarTodosLosDiscos();
+
+document.write("<br> MOSTRAMOS LOS DATOS DEL DISCO 4 A TRAVES DE GET  <br> <br>");
+
+function mostrarDiscoconGET() {
+  document.write("Nombre del disco:", disco4.Nome, "<br>"  );
+  document.write("Grupo de música o cantante:", disco4.Grupo, "<br>");
+  document.write("Año de publicación:", disco4.Ano, "<br>");
+  document.write("Tipo de música:", disco4.Tipo, "<br>");
+  document.write("¿Está prestado?", disco4.Prestado ? "Si" : "No"+  "<br>");
+}
+
+mostrarDiscoconGET();
+
+
+document.write("<br> CREAMOS UN DISCO NUEVO A TRAVES DE SET <br> <br>");
+
+
+function crearDiscoconSET(){
+
+
+}
+
+crearDiscoconSET(); 
