ab1efacc0823d3ff68fe75c11b1af7f5717024b1
maria 1 <A19MariaGA@iessanclemente.net>
2024-02-20 17:38:58 +0100

    ejercicio1 ud06

---
+++ DWCC-06/Ejercicio1/Ejercicio1-06.js
@@ -0,0 +1,42 @@
+
+
+//O número de parágrafos da páxina.
+
+    let numb = document.getElementsByTagName("p").length;
+    console.log("El número total de parrafos es: " + numb );
+
+//O texto do segundo parágrafo.
+
+    let segParrafo= document.getElementsByTagName("p")[1].childNodes[0].nodeValue;
+    console.log("El segundo parrafo dice: " + segParrafo );
+
+
+//O número de ligazóns da páxina.    
+    let numeroEnlaces= document.getElementsByTagName("a").length;
+    console.log("El numero de enlaces de la web es: " + numeroEnlaces );
+
+
+//Enderezo da primeira ligazón. 
+    let primerSrc = document.getElementsByTagName("a")[0].href;
+    console.log("La direccion del primer enlace es: " + primerSrc );
+
+
+//Enderezo da penúltima ligazón.
+
+let penultimoSrc =  document.getElementsByTagName('a');
+
+penultimoSrc =document.getElementsByTagName('a')[penultimoSrc.length - 2].href;
+console.log("La direccion del penúltima enlace es: " + penultimoSrc );
+
+//Número de ligazóns do primeiro parágrafo
+ 
+
+let enlPriParrafo = document.getElementsByTagName("p")[0].getElementsByTagName("a").length;
+console.log("Número de ligazóns do primeiro parágrafo: " + enlPriParrafo);
+
+
+   
+
+
+
+
