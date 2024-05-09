05e822681630e0aa052913c88bc335d92782c630
maria 1 <A19MariaGA@iessanclemente.net>
2024-05-09 16:35:40 +0200

    transiciones

--- DWCC-08/tarea8.js
+++ DWCC-08/tarea8.js
@@ -3,7 +3,14 @@ const App = {
         return {
             Nombre: 'A Tenda do Barrio',
             ok: true,
+            existe:false,
+            nombre: '',
+            direccionPersonal: '',
             hora: new Date().getHours(),
+            size:20,
+            colorVal: 100,
+
+
             productos: [
                 { nombre: 'Brocoli', precio: 2, imagen: 'brocoli.jpeg' },
                 { nombre: 'Coliflor', precio: 2.5, imagen: 'coliflor.jpeg' },
@@ -18,8 +25,27 @@ const App = {
     methods: {
         direccion() {
             alert("Republica del Salvador nº25");
-        }
+        },
+
+
+        
+    enviarPedido() {
+        // Aquí puedes manejar el envío del pedido, por ejemplo, mediante una llamada a una API
+        console.log("Pedido enviado:", { nombre: this.nombre, direccion: this.direccion });
+        // Luego puedes reiniciar el formulario si es necesario
+        this.nombre = '';
+        this.direccion = '';
+        // También puedes cerrar el formulario si lo deseas
+        this.existe = false;
+      }
+
+
     }
+
+
+
+
+
 }
 
 Vue.createApp(App).mount('#app');
