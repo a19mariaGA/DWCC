072d7bd3e67732989a40f7ea7dfd1b62c2b28c31
maria 1 <A19MariaGA@iessanclemente.net>
2024-05-16 19:24:43 +0200

    ultimo borrador

--- DWCC-08/tarea8.js
+++ DWCC-08/tarea8.js
@@ -11,7 +11,13 @@ const App = {
             colorVal: 100,
 
 
-            
+            productos: [
+                { nombre: 'Brocoli', precio: 2, imagen: 'brocoli.jpeg' },
+                { nombre: 'Coliflor', precio: 2.5, imagen: 'coliflor.jpeg' },
+                { nombre: 'Pimientos', precio: 1.23, imagen: 'pimientos.jpeg' },
+                { nombre: 'Ajos', precio: 2, imagen: 'ajos.jpeg' },
+                { nombre: 'Zanahorias', precio: 3, imagen: 'zanahorias.jpeg' }
+            ] 
 
         }
     },
